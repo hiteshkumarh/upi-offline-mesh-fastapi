c953c164e4d80576aa26417a9802a1c0c84bf081
hithesh kumar H <152245538+hiteshkumarh@users.noreply.github.com>
2026-06-13 22:40:33 +0530

    Update dashboard_controller.py

--- routers/dashboard_controller.py
+++ routers/dashboard_controller.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
 
-router = APIRouter()
+router = APIRouter() 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "..", "templates"))
 
 @router.get("/", response_class=HTMLResponse)
