c3301e5d88239ce0529af61c1230849a6d96a38d
Hithesh kumar H <152245538+hiteshkumarh@users.noreply.github.com>
2026-07-25 15:12:17 +0530

    Fix formatting issues in dashboard_controller.py

--- routers/dashboard_controller.py
+++ routers/dashboard_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import os
-    
+          
 router = APIRouter() 
 templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "..", "templates"))   
 
